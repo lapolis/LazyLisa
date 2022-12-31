24ded9999247938d13ee2b5d5be393f66b2d0ef6
lapolis <57040859+lapolis@users.noreply.github.com>
2022-12-31 08:39:11 +0000

    Improved Insta login using session file

--- main.py
+++ main.py
@@ -27,34 +27,46 @@ def send_msg(msg):
 		method  = 'sendMessage'
 		requests.post( url=f'https://api.telegram.org/bot{telegram_token}/{method}', data={'chat_id': telegram_chat_id, 'text': msg} ).json()
 
-def logit(msg):
+def logit(msg, send=0):
 	print(f'{time.strftime("%Y/%m/%d-%H:%M:%S")} - {msg}')
+	if send:
+		send_msg(msg)
 
-def get_latest_post(path, user, passwd, target_profile):
+def insta_login(path, user, passwd):
 	insta = instaloader.Instaloader(sanitize_paths=True,dirname_pattern=path)
 	
 	try:
-		insta.login(user, passwd)
+		insta.load_session_from_file(user)
+		return insta
+	except Exception as e:
+		logit(f'Session file for user {user} is not present.')
+
+	try:
+		if passwd:
+			insta.login(user, passwd)
+		else:
+			insta.interactive_login(user)
 	except Exception as e:
 		msg = f'Insta broke at login -> {e}'
-		logit(msg)
-		send_msg(msg)
+		logit(msg, 1)
+		insta = False
 
+	return insta
+
+def get_latest_post(path, target_profile, insta):
 	## requesting a password
 	# insta.interactive_login(user)
 	try:
 		profile = Profile.from_username(insta.context, target_profile)
 	except Exception as e:
 		msg = f'Insta getting profile broke -> {e}'
-		logit(msg)
-		send_msg(msg)
+		logit(msg, 1)
 
 	try:
 		posts = profile.get_posts()
 	except Exception as e:
 		msg = f'Insta getting post broke -> {e}'
-		logit(msg)
-		send_msg(msg)
+		logit(msg, 1)
 
 	for p in posts:
 		# print(p.title)
@@ -83,8 +95,7 @@ def get_latest_post(path, user, passwd, target_profile):
 					post_down_bool = insta.download_post(p, target=profile)
 				except Exception as e:
 					msg = f'Insta downloading post failed -> {e}'
-					logit(msg)
-					send_msg(msg)
+					logit(msg, 1)
 					post_down_bool = False
 
 				return post_down_bool
@@ -185,8 +196,7 @@ def tweet_it(post_content, api_key, api_secret, access_token, access_token_secre
 		ret = api.update_status(status=post_arr[0], media_ids=media_ids, possibly_sensitive=False, lat=+51.50, long=-0.11, display_coordinates=False)
 	except Exception as e:
 		err = f'Twitter failed with error: {e}'
-		logit(err)
-		send_msg(err)
+		logit(err, 1)
 		return 1
 
 	time.sleep(5)
@@ -199,9 +209,7 @@ def tweet_it(post_content, api_key, api_secret, access_token, access_token_secre
 			time.sleep(5)
 
 	msg = f'New tweet is up -> https://twitter.com/{screen_name}/status/{tweetid}'
-	logit(msg)
-	send_msg(msg)
-
+	logit(msg, 1)
 
 def tumblr_post_it(post_content, consumer_key, consumer_secret, oauth_token, oauth_secret):
 	client = pytumblr2.TumblrRestClient(consumer_key, consumer_secret, oauth_token, oauth_secret)
@@ -245,8 +253,7 @@ def tumblr_post_it(post_content, consumer_key, consumer_secret, oauth_token, oau
 
 	post_id = client.posts(account_name)['posts'][0]['id']
 	msg = f'Tumblr post done -> https://www.tumblr.com/{account_name}/{post_id}'
-	logit(msg)
-	send_msg(msg)
+	logit(msg, 1)
 
 def pin_it(post_content, app_id, app_secret):
 	# 500 chars limit
@@ -280,7 +287,10 @@ def main():
 
 	if 'Instagram' in config:
 		INSTA_USER = config['Instagram']['user']
-		INSTA_PASS = config['Instagram']['pass']
+		if 'pass' in config['Instagram']:
+			INSTA_PASS = config['Instagram']['pass']
+		else:
+			INSTA_PASS = ''
 		target_profile = config['Instagram']['target_profile']
 	else:
 		logit('XX Missing Insta creds')
@@ -319,34 +329,36 @@ def main():
 		except Exception as e:
 			logit(f'Pinterest config is broken with error: {e}')
 
+	insta = insta_login(post_fold, INSTA_USER, INSTA_PASS)
+	if not insta:
+		exit(1)
+
 	msg = f'LazyLisa started!\nTumblr: {tumblr}\nTwitter: {tweet}\nPinterest: {pin}'
 	send_msg(msg)
 
 	while True:
 		try:
-			new_post = get_latest_post(post_fold, INSTA_USER, INSTA_PASS, target_profile)
+			new_post = get_latest_post(post_fold, target_profile, insta)
 		except Exception as e:
 			msg = f'Something broke down with Instagram -> {e}'
-			logit(msg)
-			send_msg(msg)
+			logit(msg, 1)
 			new_post = False
 
 		# if True:
 			# logit('NOT - DEBUG - Got latest post')
 		if new_post:
-			logit('Got latest post')
-			send_msg('Got latest post, posting soon.')
+			logit('Got latest post, posting soon.', 1)
 			post_content = file_check(post_fold)
 
 			if tweet:
 				logit('Tweeting now!')
-				tweet_it(post_content, twitter_API, twitter_API_SECRET, twitter_TOKEN, twitter_TOKEN_SECRET)
+				# tweet_it(post_content, twitter_API, twitter_API_SECRET, twitter_TOKEN, twitter_TOKEN_SECRET)
 			if pin:
 				logit('Posting on Pinterest')
 				# pin_it(post_content, pin_APP_ID, pin_APP_SECRET, pin_BOARD_ID)
 			if tumblr:
 				logit('Posting on Tumblr')
-				tumblr_post_it(post_content, tumblr_CUSTOMER_KEY, tumblr_CUSTOMER_SECRET, tumblr_OAUTH_TOKEN, tumblr_OAUTH_SECRET)
+				# tumblr_post_it(post_content, tumblr_CUSTOMER_KEY, tumblr_CUSTOMER_SECRET, tumblr_OAUTH_TOKEN, tumblr_OAUTH_SECRET)
 
 		else:
 			logit('Noting to do now')
